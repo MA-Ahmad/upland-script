601bece4702aac713200b5340bd38497ed0044ba
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2023-07-31 23:24:55 +0500

    Update containerProcessing.js

--- containerProcessing.js
+++ containerProcessing.js
@@ -1,64 +1,62 @@
 const cron = require('node-cron')
 const axios = require('axios')
 
-cron.schedule('0 */3 * * *', () => {
-  const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false })
-  console.log(`Cron job running every 3 hours. Current time: ${currentTime}`)
-})
-
-// const DEV_SHOP_ID = 'edadafdf-c213-462b-910a-179c18d3a7cd'
-
-// cron.schedule('0 9 * * *', async () => {
-//   try {
-//     // Fetch data from the API
-//     const response = await axios.get('https://api.example.com/data')
-
-//     // Process the data
-//     if (response.status === 200) {
-//       const data = response.data
-
-//       const createdDate = new Date(data.created_at)
-//       const currentDate = new Date()
-
-//       // Calculate the difference in days
-//       const diffInDays = Math.floor(
-//         (currentDate - createdDate) / (1000 * 60 * 60 * 24)
-//       )
-
-//       // Check if the object is 10 days old and has an active status
-//       if (diffInDays >= 10 && data.status === 'active') {
-//         // Call the resolve API
-//         if (data.containerId) {
-//           try {
-//             const resolveResponse = await axios.post(
-//               `/containers/${data.containerId}/resolve`,
-//               data.metaData
-//             )
-//             console.log(
-//               `Resolve API called successfully for object: ${data.name}`
-//             )
-
-//             if (resolveResponse.status === 200) {
-//               await axios.post('/containers', {
-//                 devShopId: DEV_SHOP_ID,
-//               })
-//               console.log(
-//                 `Containers API called successfully for object: ${data.name}`
-//               )
-//             } else {
-//               console.error(
-//                 `Resolve API did not return a successful status code for object: ${data.name}`
-//               )
-//             }
-//           } catch (error) {
-//             console.error(`Error calling resolve API for object: ${data.name}`)
-//           }
-//         } else {
-//           console.warn(`No containerId available for object: ${data.name}`)
-//         }
-//       }
-//     }
-//   } catch (error) {
-//     console.error('Error:', error)
-//   }
+// cron.schedule('0 */3 * * *', () => {
+//   const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false })
+//   console.log(`Cron job running every 3 hours. Current time: ${currentTime}`)
 // })
+
+const API_URL =
+  'https://2amhjh2kla.execute-api.us-west-1.amazonaws.com/prod/backend'
+const SECRET_KEY = '25599550-2686-4879-b83c-ee5ce88cff00'
+
+cron.schedule('* * * * *', async () => {
+  try {
+    const response = await axios.post(API_URL, {
+      type: 'current_escrow_timestamp',
+      secret_key: SECRET_KEY,
+    })
+
+    // Process the data
+    if (response.status === 200) {
+      const data = response.data
+
+      const createdDate = new Date(data.body.created_on * 1000)
+      const currentDate = new Date()
+
+      // Calculate the difference in days
+      const diffInDays = Math.floor(
+        (currentDate - createdDate) / (1000 * 60 * 60 * 24)
+      )
+
+      console.log('response', createdDate, currentDate, diffInDays)
+      // Check if the object is 10 days old and has an active status
+      if (diffInDays >= 0) {
+        // Call the resolve API
+        try {
+          const resolveResponse = await axios.post(API_URL, {
+            type: 'resolve_escrow',
+            secret_key: SECRET_KEY,
+          })
+
+          console.log('=== resolve_escrow API called successfully ===')
+          if (resolveResponse.status === 200) {
+            await axios.post(API_URL, {
+              type: 'create_escrow',
+              secret_key: SECRET_KEY,
+            })
+            console.log('=== create_escrow API called successfully ===')
+          } else {
+            console.error(
+              '=== resolve_escrow API did not return a successful status code ==='
+            )
+          }
+        } catch (error) {
+          console.error('=== Error calling resolve_escrow API ===')
+        }
+      }
+    }
+  } catch (error) {
+    console.error('Error:', error)
+  }
+})
